26d50fed6747aca3b313cfad93e20ccae790ee5c
ovidio <omorales@lacarlota.um.edu.mx>
2013-11-13 20:10:42 -0600

    inicio

--- protected/views/catalogos/index.php
+++ protected/views/catalogos/index.php
@@ -39,15 +39,11 @@ document.getElementById(id).style.background ="";
 
 <ul class="nav nav-tabs" id="myTab">
 
-    <li><a href="#altaOrden" name="altaOrden"><small>Nueva </small> <span class="glyphicon glyphicon-plus"></span></a></li>
-    <li><a href="#pendientes" name="pendientes"><small>Pendientes </small><span class="glyphicon glyphicon-list-alt"></span>
+    <li><a href="#clientes" name="clientes"><small>Clientes</small> <span class="glyphicon glyphicon-briefcase"></span></a></li>
+    <li><a href="#usuarios" name="usuarios"><small>Usuarios <span class="glyphicon glyphicon-user"></span>
+</small>
 </a></li>
-    <li><a href="#enProceso" name="enProceso"><small>En Proceso <span class="glyphicon glyphicon-list-alt"></span>
-</small></a></li>
-    <li><a href="#terminadas" name="terminadas"><small>Terminadas <span class="glyphicon glyphicon-list-alt"></span>
-</small></a></li>
-    <li><a href="#resumen" name="resumen"><small>Resumen <span class="glyphicon glyphicon-stats"></span>
-</small></a></li>
+
    
 </ul>
 
@@ -73,7 +69,7 @@ document.getElementById(id).style.background ="";
     
     
     
-    <div class="tab-pane" id="altaOrden"><!--TAB1 #altaOrden-->
+    <div class="tab-pane" id="clientes"><!--TAB1 #altaOrden-->
             
         <br>
         <div class="panel panel-primary">
@@ -264,87 +260,188 @@ if(win.window.focus){win.window.focus();}
             
             
             
+  <div class="tab-pane" id="usuarios"><!--TAB2 #usuarios-->
             
+        <br>
+        <div class="panel panel-primary">
             
-            
-            
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-          
-        
-<div class="tab-pane" id="pendientes"><!--TAB2 #pendientes-->
-    
-  
-    
-    <br>
-
-
-             <!--pendientes-->    
-         <div class="panel panel-primary">
-                <div class="panel-heading"></div>     
-
-
-
-          <table  width="800"  class="table table-condensed table-hover" >
-
-
-              <form>
-
-
- 
-                  
-                  
-                  
-                  
                  
+                 
+                 
+
+
+
+      <div class="panel-heading"></div>   
+
+
+
+
+
+
+
+
+
+
+
+
+                <form name="formaCaptura" class="form" method="post"  >
+
+
+          
+
+
+
+
+                    
+       
+                    
+
+
+          <table width="800" class="table table-condensed table-hover" >
+
+              
+
+    
+               
+
+
+
+
+       <tr>
+              <td width="152" scope="col"><div align="left"><h6>Usuario</h6></div></td>
+              <td width="451" scope="col"><label> </label>
+                  <div align="left">
+                    <input class="form-control input-sm span2" tabindex = "1" placeholder="Usuario Solicitante" id="focusedInput" type="text" name="nombre" value=""/>
+                </div></td>
+            </tr>
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+<script>
+
+var win = null;
+function nueva(mypage,myname,w,h,scroll){
+LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+win = window.open(mypage,myname,settings)
+if(win.window.focus){win.window.focus();}
+}
+
+</script>
+
+              <tr>
+
                 
+                  
+              <td ><div  align="left"><small>Tipo Soporte</small><a href="#altaOrden" onClick="nueva('../ventanas/v_tipo_soporte.php?almacen=')">
+          <span data-toggle="tooltip" title="Agregar tipos de soporte.." class="glyphicon glyphicon-plus-sign small"></span>
+</a></div></td>
+              
+            </tr>
+
+
+
+
+
+
+
+
+
+            
+
+
+
+              <tr>
+
+              <td scope="col"><div align="left"><h6>Extension</h6></div></td>
+              <td scope="col">
+
+                <small>
+
+                    <input class="form-control input-sm span1" tabindex = "6" placeholder="" id="focusedInput" type="text" name="extension" value=""></input>
+
+                </small>
+
+              </label></td>
+            </tr>
+
+
+
+     
+
+      <tr>
+          <td width="152" scope="col"><div align="left"><h6>Usuario Ejecutor</h6></div></td>
+              <td width="451" scope="col">                  
+                  <div align="left">
+                    <input class="form-control input-sm span2" tabindex = "7" placeholder="Usuario Ejecutor" id="focusedInput" type="text" name="usuarioAplicacion" value=""/>
+                </div></td>
+            </tr> 
+           
+
+
+
+              <tr>
+              <td width="152" scope="col"><div align="left"><h6>Descripción del Servicio</h6></div></td>
+              <td width="451" >
+                  <div align="left">
+                    <textarea name="observaciones" tabindex = "8" class="form-control input-sm span2" placeholder="Observaciones" cols="50" rows="5"  /></textarea>
+                </div></td>
+            </tr> 
+
+     
+
+               <tr>
+              <td  >&nbsp;</td>
+              <td ><input class="btn btn-primary btn-sm span2" tabindex = "9" name="generaOrden" type="submit"  id="actualizar" value="Generar Orden" /></td>
+            </tr>    
 
           </table>
-            
-       
-            
-            
-            
-         </div>
-             
-   
-             
-             
-             
-             
-             
 
 
 
 
 
-
-
-             
-             
-        </div><!--DIV DEL TAB FINALIZA-->
-
+        </form>
     
-    
-    
-    
-    
-            
 
+
+
+
+
+
+
+                      </div>   
+    </div>
+    
            
             
             
@@ -361,212 +458,24 @@ if(win.window.focus){win.window.focus();}
             
             
             
+
             
             
-            <div class="tab-pane" id="enProceso"><!--TAB3 #enProceso-->
-                
-                
-                
-                
-     
-                
-
-
-
-                 <!--en proceso-->    
- 
-
-<div class="panel panel-primary">
-      <div class="panel-heading"></div>     
-    
-    
-            <table width="800"  class="table table-condensed table-hover" >
- 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
           
-
-
-              
-                      
-              
-              
-              
-              
-
-                          
-
-
-
-
-            <td>
-  <!-- Button trigger modal -->
-  
-  
-
-  
-  
-  
-  
-<!-- Modal -->
-  <div class="modal fade" id="myModal<?php echo $a3;?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title">Observaciones</h4>
-        </div>
-        <div class="modal-body">
-            
-<form name="forma1" method="post">
-
-<table  width="400" class="table table-striped"  align="center">
-    
-<tr>      
- 
-
-    
-     <td scope="col"><div align="center">
-<input type="hidden" value="" name="keySOP">
-    <input type="hidden" value="<?php //echo $_GET['entidad'];?>" name="entidad">
-<textarea name="descripcion" cols="50" rows="5"></textarea>
-              
-     
-    </div></td>
-    
-    
-
-    
-</tr>
-    <tr>
-         <td>
-    <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-          <input type="submit" class="btn btn-success" name="ADDOS" value="Guardar"></input>
-        </div>        
         
-    </td>   
-    </tr>      
-      
-  
-            
-                  
-      
-      
-
-              
-              
-         
-
-    
-    
-<tr>    
-
-  
- 
-  
-  
-  
-     <td >
- 
-       
-   </td>
-  
-    
-      
-      
-      
-      
-      
-      
-    </tr>
-    </table>
-    
-
-
-</form>
-        </div>
-        
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->    
-                
-            </td>
-
-
-                    <td >
-
-
-
-
-                  </td>
-
-
-
-                  </tr> 
-
-
-              </table>
-</div>
-                
-    
-
-
-
-
-
-
-
-
-
-
-<br>
-    <div class="alert alert-warning">
-    No existen registros!
-    </div>
-
-
-
-    </div>
-    
-    
-    
-    
-     
-            
-            
-            
-                            
-
-            
-            
-            
-            
-            
-            
-            
-
-
-                   
-
-            
-            
-            
-            
-            
-                       
-              
-    </div> 
-  
-  
-  
-  
-  
-  
-  
-    
-
-
-
-
    
 </div>    <!--div de container tabs-->
 
